9f44390460a4e911a8856fa61506668b181ef7a2
Nandy Rodrigo <nandy.rodrigo@gmail.com>
2020-07-22 15:41:07 +1000

    layout 1st draft

--- main.rb
+++ main.rb
@@ -1,9 +1,51 @@
      
 require 'sinatra'
+require 'sinatra/reloader' if development?
+# also_reload 'models/dish.rb' if development?
+# also_reload 'models/user.rb' if development?
+require 'pry'
+require 'pg'
+# require_relative 'models/dish.rb'
+# require_relative 'models/user.rb'
+
+enable :sessions
 
 get '/' do
+
   erb :index
 end
+
+get '/signup' do
+
+  erb :signup
+end
+
+get '/login' do
+
+  erb :login
+end
+
+get '/captain_questions' do
+
+  erb :captain_questions
+end
+
+get "/captain_select" do
+
+  erb :captain_select
+end
+
+get "/logout" do
+
+  erb :logout
+end
+
+get "/close_account" do
+
+  erb :close_account
+end
+
+
 
 
 
